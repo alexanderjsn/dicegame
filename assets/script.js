2d15503b6ee83faddfddc5214d93d7ca98d2ad6c
alexanderjsn <alexanderjsn@live.com>
2022-10-07 02:51:50 +0000

    added random number generator for both players  in java

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded"), function(){
 let buttons;
-let pagetitle;
-let sidetitle;
-let opponentscore;
-let playerscore;
+let playerdice = 0;
+let opponentdice = 0;
+let opponentscore = 0;
+let playerscore = 0;
 
 
-}
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        let playerdice = Math.floor(Math.random() * 6);
+            console.log(playerdice);
+        let opponentdice = Math.floor(Math.random() * 6);
+            console.log(opponentdice);
+    })}}
