50c0176b33171abd8b13e2292669c950163b379d
alexanderjsn <alexanderjsn@live.com>
2022-10-07 03:13:41 +0000

    added comments to js

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,18 @@ let opponentdice = 0;
 let opponentscore = 0;
 let playerscore = 0;
 
+function rollDice() {
+    document.getElementById("blue.jpg").style.display = "block";
+};
 
+//makes button create random numbers//
 for (let button of buttons) {
     button.addEventListener("click", function() {
         let playerdice = Math.floor(Math.random() * 6);
             console.log(playerdice);
         let opponentdice = Math.floor(Math.random() * 6);
             console.log(opponentdice);
+//adds point to highest number//
             if (playerdice > opponentdice) {
                 playerscore += 1;
             } else if (opponentdice > playerdice) {
@@ -19,12 +24,12 @@ for (let button of buttons) {
             } if (opponentdice == playerdice) {
                 opponentscore += 1; playerscore += 1;
             } 
-
+//updates scores for each player//
     document.getElementsByClassName("playerscore")[0].innerHTML = playerscore;
     document.getElementsByClassName("opponentscore")[0].innerHTML = opponentscore;
     document.getElementsByClassName("playerdice")[0].innerHTML = playerscore;
     document.getElementsByClassName("opponentdice")[0].innerHTML = playerscore;
-
+//alerts the winner/loser//
     if(playerscore == 10) {
         alert(`Congratulations, you won!`); playerscore = 0; opponentscore = 0;}
         else if (opponentscore == 10) {
